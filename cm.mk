f5fe40d31ceefa3633f2051c86e419041aac80f2
arrrghhh <arrrghhh@arrrghhh-vbox.(none)>
2013-04-27 17:07:08 -0600

    xt897c

--- cm.mk
+++ cm.mk
@@ -1,6 +1,7 @@
-# Inherit some common CM stuff.
+## Specify phone tech before including full_phone
 $(call inherit-product, vendor/cm/config/cdma.mk)
 
+# Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Enhanced NFC
@@ -11,9 +12,10 @@ TARGET_SCREEN_WIDTH := 540
 TARGET_SCREEN_HEIGHT := 960
 
 # Release name
-PRODUCT_RELEASE_NAME := asanti_c
+PRODUCT_RELEASE_NAME := xt897c
+PRODUCT_NAME := cm_xt897c
 
-$(call inherit-product, device/moto/asanti_c/full_asanti_c.mk)
+$(call inherit-product, device/motorola/xt897c/full_xt897c.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_BRAND=motorola PRODUCT_NAME=xt897 BUILD_PRODUCT=asanti_c BUILD_FINGERPRINT=motorola/XT897_us_spr/asanti_c:4.0.4/7.7.1Q-6_SPR-125_ASA-14/15:user/release-keys
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_BRAND=motorola PRODUCT_NAME=xt897c BUILD_PRODUCT=xt897c BUILD_FINGERPRINT=motorola/XT897_us_spr/asanti_c:4.1.2/9.8.2Q-122_XT897_FFW-5/6:user/release-keys
 
